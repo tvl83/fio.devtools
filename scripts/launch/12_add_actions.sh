7df64a363e87585b1cc0a7c43e0ddf74a3a350b4
Thomas Le <tvle83@gmail.com>
2020-12-23 20:48:00 -0600

    Update 12_add_actions.sh
    adding hello action

--- scripts/launch/12_add_actions.sh
+++ scripts/launch/12_add_actions.sh
@@ -46,3 +46,6 @@ host='localhost:8889'
 
 # USED FOR TESTING:
 ./clio -u http://$host push action eosio addaction '{"action":"migrtrx","contract":"fio.reqobt","actor":"eosio"}' --permission eosio
+
+# fio.escrow testing:
+./clio -u http://$host push action eosio addaction '{"action":"hello","contract":"fio.escrow","actor":"eosio"}' --permission eosio
